569f4769f0fee05adc24e26d75814371186aa417
wbmccurry20 <wbmccurry20@gmail.com>
2024-08-18 21:09:24 -0400

    add ssl verification testing

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask
-from app.routes import init_routes
+from flask import Flask, redirect, request
 
 def create_app():
     app = Flask(__name__, static_folder='static', template_folder='templates')
+
+    @app.before_request
+    def before_request():
+        if request.is_secure is False:
+            url = request.url.replace('http://', 'https://', 1)
+            return redirect(url, code=301)
 
     # Initialize routes
     init_routes(app)
